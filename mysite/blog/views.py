3fc65eaa2f1f7cf90893641ca75e835725658ef0
Alexandr Lukyanets <alexlukyanets18@gmail.com>
2020-11-12 11:34:05 +0200

    Ready app

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -44,7 +44,36 @@ class GetPost(DetailView):
         return context
 
 class PostsByTag(ListView):
-    pass
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+
+        return context
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    def get_queryset(self):
+        if self.request.GET.get('s'):
+            return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+        return ''
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["s"] = f"s={self.request.GET.get('s')}&"
+        return context
+
+
 
 def get_category(request, slug):
     return render(request, 'blog/category.html')
